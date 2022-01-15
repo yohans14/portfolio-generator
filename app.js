d4e9f2cf169aa12938573df506e3f6ebbf521b83
yohans14 <yohans14@yahoo.com>
2022-01-15 04:48:09 -0600

    achieved the first question prompt

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template");
+const inquirer = require("inquirer");
+// //requiring core libarary in Node.js
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
-
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "Name",
+      message: "whats is your name?",
+    },
+  ])
+  .then((answer) => console.log(answer));
