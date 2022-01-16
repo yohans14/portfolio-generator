616ed62422b69627f3c8c8c49794769cf02ca64f
yohans14 <yohans14@yahoo.com>
2022-01-15 21:27:00 -0600

    allow user to enter multiple project

--- app.js
+++ app.js
@@ -47,95 +47,96 @@ const promptProject = (portfolioData) => {
   Add a New Project
   =================
   `);
-  return (
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is the name of your project? (Required)",
-          validate: (nameInput) => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log("Please enter your name!");
-              return false;
-            }
-          },
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the name of your project!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "description",
-          message: "Provide a description of the project (Required)",
-          validate: (nameInput) => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log("Please enter your name!");
-              return false;
-            }
-          },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
         },
-        {
-          type: "checkbox",
-          name: "languages",
-          message:
-            "What did you build this project with? (Check all that apply)",
-          choices: [
-            "JavaScript",
-            "HTML",
-            "CSS",
-            "ES6",
-            "jQuery",
-            "Bootstrap",
-            "Node",
-          ],
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (Check all that apply)",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Enter the GitHub link to your project. (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
         },
-        {
-          type: "input",
-          name: "link",
-          message: "Enter the GitHub link to your project. (Required)",
-          validate: (nameInput) => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log("Please enter your name!");
-              return false;
-            }
-          },
-        },
-        {
-          type: "confirm",
-          name: "feature",
-          message: "Would you like to feature this project?",
-          default: false,
-        },
-        {
-          type: "confirm",
-          name: "confirmAddProject",
-          message: "Would you like to enter another project?",
-          default: false,
-        },
-      ])
-      // If there's no 'projects' array property, create one
-      .then((projectData) => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-          return promptProject(portfolioData);
-        } else {
-          return portfolioData;
-        }
-      })
-  );
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
 };
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
   });
+
+// will be uncommented in lesson 4
+// const pageHTML = generatePage(portfolioData);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+//   console.log('Page created! Check out index.html in this directory to see it!');
+// });
